3e30246fcc394194a37587015ff8e8be40292c45
Sadia Siddiqua <siddiqua.sadiaa@outlook.com>
2018-06-28 17:18:24 +0200

    Testing
    Testing

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> testing template</title>
+  </head>
+  <body>
+
+    <?php
+include "Comdirect/Includes/header.php";
+
+?>
+
+  </body>
+</html>
