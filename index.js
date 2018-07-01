886b80de066f4a904dc6e4c8887c6a59fdb4f477
Sadia Siddiqua <siddiqua.sadiaa@outlook.com>
2018-07-01 12:14:29 +0200

    Comdirect sample page

---
+++ index.js
@@ -0,0 +1,22 @@
+// $('input[type="submit"]').click(function () {
+//
+//   $('#myform').validate({ // initialize the plugin
+//           rules: {
+//               Fname: {
+//                   required: true,
+//                   email: true
+//               },
+//               Lname: {
+//                   required: true,
+//
+//               },
+//               email: {
+//                   required: true,
+//                   email: true
+//           }
+//         }
+//       });
+//
+//
+//
+// });
